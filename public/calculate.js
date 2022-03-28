67c0e0ed91a69314f023ba64118e1f799b491e04
frastsytt <frastsytt@gmail.com>
2022-03-28 09:50:33 +0300

    As a user I am able to input the item, the quantity of the items and my two-letter state code [Delivers #181649691]

--- public/calculate.js
+++ public/calculate.js
@@ -1,5 +1,37 @@
-console.log('aaaa')
+const states = [
+    {state: 'UT', tax: 6.85},
+    {state: 'NV', tax: 8.00},
+    {state: 'TX', tax: 6.25},
+    {state: 'AL', tax: 4.00},
+    {state: 'CA', tax: 8.25},
+];
+
+const discounts = [
+    {value: 50000, discount: 15},
+    {value: 10000, discount: 10},
+    {value: 7000, discount: 7},
+    {value: 5000, discount: 5},
+    {value: 1000, discount: 3},
+    {value: 0, discount: 0}
+    ]
+
+console.log(discounts.length)
 
 function discountcalc(){
-    console.log('bbb')
+    var price = document.getElementById("price").value;
+    var quant = document.getElementById("quant").value;
+    var inputState = document.getElementById("state").value;
+    const taxResult = states.find( ({ state }) => state === inputState);
+    var tax = taxResult.tax / 100;
+    var sum = price * quant
+
+    for (let [index, el] of discounts.entries()) {
+        if (sum >= el.value) {
+            document.getElementById("postdisc").innerText = sum * ((100 - el.discount) / 100)
+            document.getElementById("posttax").innerText = sum * ((100 - el.discount) / 100) * (1 + tax)
+            break;
+        }
+    }
+    document.getElementById("pretax").innerText = sum
+
 }
